6faa9e3e42d63a9e233bb4f47593389af7e3dd2f
Stenio Araujo <stenioaraujo@lsd.ufcg.edu.br>
2018-02-28 14:59:49 -0300

    Adding _clock
    Change-Id: Id0e58c7473946590674f6b15b187b15961db8482

---
+++ _clock.py
@@ -0,0 +1,58 @@
+"""All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may
+    not use this file except in compliance with the License. You may obtain
+    a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+    License for the specific language governing permissions and limitations
+    under the License.
+"""
+
+import time
+
+
+class StopWatch:
+    """Stop Watch class"""
+
+    def __init__(self):
+        self.hours = 0
+        self.minutes = 0
+        self.seconds = 0
+
+    def start(self):
+        """Start the counter"""
+        while True:
+            text = "{:02}:{:02}:{:02}".format(
+                self.hours, self.minutes, self.seconds)
+            print(text, end="r", flush=True)
+            self.count()
+            time.sleep(1)
+
+    def count(self):
+        """Increment seconds by one, and increment all the rest"""
+        self.seconds += 1
+        if self.seconds >= 60:
+            self.seconds = 0
+
+            self.minutes += 1
+            if self.minutes >= 60:
+                self.minutes = 0
+                self.hours += 1
+
+    def __str__(self):
+        result = ("\n"
+                  "Hours: {}\n"
+                  "Minutes: {}\n"
+                  "Seconds: {}")
+
+        print(result.format(self.hours, self.minutes, self.seconds))
+
+
+if __name__ == "__main__":
+    WATCH = StopWatch()
+    WATCH.start()
